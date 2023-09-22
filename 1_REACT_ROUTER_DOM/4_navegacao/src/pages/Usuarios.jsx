b419dbb532757bd067511f1cc8bd1e32726d3ccf
Bryan Hilario <bryan.hilario10@gmail.com>
2023-09-22 15:27:38 -0300

    populando git hub

---
+++ 1_REACT_ROUTER_DOM/4_navegacao/src/pages/Usuarios.jsx
@@ -0,0 +1,15 @@
+import { Link } from  'react-router-dom'
+
+
+function Usuarios() {
+  return (
+    <div>
+        <h1>
+            Usuários
+        </h1>
+        <Link to={"/"}>Ir para Home</Link>
+    </div>
+  )
+}
+
+export default Usuarios
